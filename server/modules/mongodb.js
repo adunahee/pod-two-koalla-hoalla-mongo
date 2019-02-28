e6b0dafea1ae064eb95a55436d40d9cf76a92cd4
Anthony D <dunahee@gmail.com>
2019-02-28 13:45:25 -0600

    set up mongodb connection

---
+++ server/modules/mongodb.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+const PORT = process.env.PORT || 27017;
+const dbName = 'koalaholla';
+
+const dbUrl = `mongodb://localhost:${PORT}/${dbName}`;
+
+module.exports = () => {
+    mongoose.connect(dbUrl, { useNewUrlParser: true });
+
+    mongoose.connect.on('error', (e) => {
+        console.log('error with connection', e);
+    })
+
+    mongoose.connect.once('connected', () => {
+        console.log('mongo db connected on', PORT);
+    })
+}
