31fde85d4966a92f189014d0933c69967a8f829b
Dion Roloff <dion.roloff@gmail.com>
2019-02-28 13:49:34 -0600

    this will add components

---
+++ src/components/KoalaForm/KoalaForm.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+class KoalaForm extends Component {
+    //fill this out with required fields
+    state = { 
+
+    }
+
+    //write POST request
+
+    render() {
+        return(
+            <div>
+                <h1>Form</h1>
+            </div>
+        )
+    }
+}
+
+export default KoalaForm;
