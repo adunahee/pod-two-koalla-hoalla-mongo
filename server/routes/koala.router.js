6e3d37391de948595bd92065be76afc7b16361f3
Kye Berkeland <kyeberkeland@yahoo.com>
2019-02-28 14:05:15 -0600

    Create server GET route for koalas

--- server/routes/koala.router.js
+++ server/routes/koala.router.js
@@ -10,16 +10,23 @@ const Koala = require('../modules/Koala');
 // GET Route
 router.get('/', (req, res) => {
     // REPLACE THIS with mongoose find
-    res.send([
-        {
-            _id: ObjectId("58de9673d0c1cb22fbcb2655"),
-            name: "Scotty",
-            gender: "M",
-            age: 4,
-            ready_to_transfer: true,
-            notes: "Born in Guatemala"
-        }
-    ])
+    Koala.find({}).then((results) => {
+        res.send(results);
+    }).then((error) => {
+        console.log(`Error in GET: ${error}`);
+        res.sendStatus(500);
+    });
+
+    // res.send([
+    //     {
+    //         _id: ObjectId("58de9673d0c1cb22fbcb2655"),
+    //         name: "Scotty",
+    //         gender: "M",
+    //         age: 4,
+    //         ready_to_transfer: true,
+    //         notes: "Born in Guatemala"
+    //     }
+    // ])
 });
 
 // POST route
